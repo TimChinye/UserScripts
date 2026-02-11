ec971b05807587da48b48b6111c0361eb4566d8c
Tim <150863066+TimChinye@users.noreply.github.com>
2026-02-11 19:26:10 +0000

    Bump version from 1.0.3 to 1.0.4

--- MooMoo.io Utility Mod/script.user.js
+++ MooMoo.io Utility Mod/script.user.js
@@ -4,7 +4,7 @@
 // @description  Enhances MooMoo.io with mini-mods to level the playing field against cheaters whilst being fair to non-script users.
 // @license      GNU GPLv3 with the condition: no auto-heal or instant kill features may be added to the licensed material.
 // @author       TigerYT
-// @version      1.0.3
+// @version      1.0.4
 // @grant        GM_info
 // @match        *://moomoo.io/*
 // @match        *://dev.moomoo.io/*
